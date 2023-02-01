7e5d5a578b371e771b803e33702c939bf36dfe7b
Vladimir <Vladimir.Stognij@gmail.com>
2023-02-01 23:55:52 +0200

    ...

--- src/Screens/RegistrationScreen.js
+++ src/Screens/RegistrationScreen.js
@@ -1,12 +1,11 @@
 import {
-  StyleSheet,
   Text,
   View,
   ImageBackground,
   TextInput,
-  Button,
   TouchableOpacity,
   Image,
+  StyleSheet,
 } from "react-native";
 
 export default function RegistrationScreen() {
@@ -18,32 +17,47 @@ export default function RegistrationScreen() {
       >
         <View style={styles.form}>
           <View style={styles.avatar}>
-            <TouchableOpacity activeOpacity={0.7}>
-              <View style={styles.addButton}>
-                <Image
-                  style={styles.addButtonIcon}
-                  source={require("../../assets/img/add.svg")}
-                />
-              </View>
+            <TouchableOpacity activeOpacity={0.8} style={styles.addButton}>
+              <Image
+                style={styles.addButtonIcon}
+                source={require("../../assets/img/add.svg")}
+              />
             </TouchableOpacity>
           </View>
 
-          <Text style={styles.formTitle}>Регистрация</Text>
-          <View>
-            <Text>Логин</Text>
-            <TextInput />
+          <Text style={styles.formTitle}>Реєстрація</Text>
+
+          <View style={styles.inputsWrapper}>
+            <TextInput
+              style={styles.input}
+              // onChangeText={ }
+              // value={ }
+              placeholder="Логін"
+            />
+            <TextInput
+              style={styles.input}
+              // onChangeText={ }
+              // value={ }
+              placeholder="Адреса електронної пошти"
+            />
+            <View style={styles.passwordWrapper}>
+              <TextInput
+                style={styles.input}
+                // onChangeText={ }
+                // value={ }
+                placeholder="Пароль"
+              />
+              <Text style={styles.showPasswordBtn}>Показать</Text>
+            </View>
           </View>
-          <View>
-            <Text>Адрес электронной почты</Text>
-            <TextInput />
-          </View>
-          <View>
-            <Text>Пароль</Text>
-            <TextInput />
-            <Text>Показать</Text>
-          </View>
-          <Button title="Зарегистрироваться" />
-          <Text>Уже есть аккаунт? Войти</Text>
+
+          <TouchableOpacity activeOpacity={0.8} style={styles.regButton}>
+            <Text style={styles.regBtnTitle}>Зареєструватись</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity activeOpacity={0.8}>
+            <Text style={styles.redirectLink}>Уже есть аккаунт? Войти</Text>
+          </TouchableOpacity>
         </View>
       </ImageBackground>
     </View>
@@ -65,6 +79,7 @@ const styles = StyleSheet.create({
     paddingTop: 92,
     paddingLeft: 16,
     paddingRight: 16,
+    paddingBottom: 76,
     backgroundColor: "#FFFFFF",
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
@@ -92,7 +107,53 @@ const styles = StyleSheet.create({
     marginBottom: 32,
     fontWeight: 600,
     fontSize: 30,
-    lineHeight: 1.67,
+    lineHeight: "1.67",
     textAlign: "center",
+  },
+  inputsWrapper: {
+    marginBottom: 42,
+    display: "flex",
+    gap: 16,
+  },
+  input: {
+    padding: 16,
+    height: 44,
+    backgroundColor: "#f6f6f6",
+    border: "1px solid #e8e8e8",
+    borderRadius: 8,
+    fontSize: 16,
+  },
+  passwordWrapper: {
+    position: "relative",
+    marginBottom: 0,
+  },
+  showPasswordBtn: {
+    position: "absolute",
+    right: 16,
+    top: "50%",
+    transform: "translateY(-50%)",
+    fontSize: 16,
+    color: "#1B4371",
+    textAlign: "right",
+  },
+  regButton: {
+    display: "flex",
+    justifyContent: "center",
+    paddingHorizontal: 16,
+    paddingVertical: 32,
+    marginBottom: 16,
+    height: 51,
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+  },
+  regBtnTitle: {
+    fontSize: 16,
+    textAlign: "center",
+    color: "#ffffff",
+  },
+  redirectLink: {
+    fontSize: 16,
+    textAlign: "center",
+    color: "#1B4371",
   },
 });
