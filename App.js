2aa523cac6926203979ab314f64d4592c61e9301
VladimirSt13 <Vladimir.Stognij@gmail.com>
2023-02-20 13:46:27 +0200

    fonts connected

--- App.js
+++ App.js
@@ -1,36 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState, useCallback } from "react";
+import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import * as SplashScreen from "expo-splash-screen";
-
-import * as Font from "expo-font";
+import { useFonts } from "expo-font";
 
 import { useRoute } from "./src/router";
 
 SplashScreen.preventAutoHideAsync();
 
-const loadFonts = async () => {
-  await Font.loadAsync({
-    "Roboto-Regular": require("./assets/fonts/Roboto/Roboto-Thin.ttf"),
+const App = () => {
+  const [isAppReady, setIsAppReady] = useState(false);
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
     "Roboto-Bold": require("./assets/fonts/Roboto/Roboto-Bold.ttf"),
     "Roboto-Medium": require("./assets/fonts/Roboto/Roboto-Medium.ttf"),
   });
-};
 
-const App = () => {
   useEffect(() => {
     async function prepare() {
       try {
-        await loadFonts();
-        await new Promise((resolve) => setTimeout(resolve, 2000));
         await SplashScreen.hideAsync();
+        console.log("Splash screen hidden");
       } catch (e) {
         console.warn(e);
       } finally {
-        await SplashScreen.hideAsync();
+        setIsAppReady(true);
       }
     }
+
     prepare();
   }, []);
+
+  if (!isAppReady || !fontsLoaded) {
+    console.log("Still loading fonts or app is not ready");
+    return null;
+  }
 
   const routing = useRoute(false);
 
